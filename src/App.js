18ee27be370d1bba14ceef83949667e59ca21411
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-03-25 21:23:40 -0400

    Tailwind CSS setup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
+        <h1 className="text-red-500">
           Cuitlazotlac Portfolio
         </h1>
         <p>
