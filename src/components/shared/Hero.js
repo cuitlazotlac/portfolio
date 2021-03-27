c0ed46c94f24227896138358af540f347028db4f
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-03-26 23:33:24 -0400

    finishing the header + hero component

---
+++ src/components/shared/Hero.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+  return (
+    <div className="bg-white h-screen flex flex-col justify-center items-center">
+      <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14">
+        MY PORTFOOOOOLIO
+      </h1>
+      <Link className="py-6 px-10 bg-yellow-500 rounded-full text-3xl hover:bg-yellow-300 transition duration-300 ease-in-out flex items-center animate-bounce">
+        Contact Me !
+        <svg
+          className="w-6 h-6 ml-4"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+          />
+        </svg>
+      </Link>
+    </div>
+  );
+};
+
+export default Hero;
