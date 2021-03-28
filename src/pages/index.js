62b66ade13cdfdd360dabf12ef4356dbeb527c84
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-03-27 21:20:38 -0400

    navbar responsive + dropdown

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Content from '../components/shared/Content'
+import Hero from '../components/shared/Hero'
+
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <Content />
+        </>
+    )
+}
+
+export default Home
