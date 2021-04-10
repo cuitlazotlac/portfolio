705b30e3fd836d491d3ac1feb4c23446022b0d6d
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-04-09 23:47:06 -0400

    adding subTitle component

---
+++ src/components/shared/SubTitle.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Typical from "react-typical";
+
+function SubTitle({ title_1, title_2, title_3,span}) {
+
+  return (
+    <div className="Title">
+            {/* <div className="SubTitle"> */}
+      <p>
+        {/* {title} */}
+        <Typical
+          loop={Infinity}
+          wrapper="b"
+          steps={[
+            title_1,
+            5000,
+            title_2,
+            5000,
+            title_3,
+            5000,
+          ]}
+        />
+        <span>{span}</span>
+      </p>
+    </div>
+  );
+}
+
+export default SubTitle;
